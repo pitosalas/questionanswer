1274e1f0f766adb3043992d5534f0aac7df2093c
Pito Salas <pitosalas@gmail.com>
2022-10-14 11:07:15 -0400

    fall

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -6,4 +6,9 @@ class AnswerTest < ActiveSupport::TestCase
     a = q.answers.create(body: "This is a test answer")
     assert a.valid?
   end
+
+  test "all amswers belong to some question" do
+    a = Answer.create(body: "Test Body")
+    refute a.valid?
+  end
 end
