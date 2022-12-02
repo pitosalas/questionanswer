1c1a7e56d8053ab9060d9345d064e9a403e8d098
Pito Salas <pitosalas@gmail.com>
2022-12-02 07:32:04 -0500

    added fortune

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# nethttp.rb
+require 'uri'
+require 'net/http'
+require 'json'
+
 module ApplicationHelper
+  def random_fortune()
+    uri = URI("https://fortune.onrender.com/fortune/random")
+    res = Net::HTTP.get_response(uri)
+    JSON.parse(res.body)["fortune"]
+  end
 end
